3db2d81d208f2bd59872d65c20e77422cabdfd02
kharola97 <akharola4@gmail.com>
2023-04-17 11:03:11 +0530

    server

--- server.js
+++ server.js
@@ -13,6 +13,19 @@ require("dotenv").config()
 const cookieParser = require('cookie-parser');
 
 app.use(cookieParser());
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "X-Requested-With,content-type"
+    );
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    next();
+  });
 
 
 
